2fab0393d906a8683246a3f3d0e78e633dd8d6ee
Stark <prateek_garg@jp.honda>
2023-08-06 13:32:12 +0900

    verified and optimized code a little, benchmark: 40 mins on openwebtext

--- src/run.cc
+++ src/run.cc
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <fstream>
 #include <chrono>
+#include <filesystem>
 
 using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
 
@@ -10,21 +11,32 @@ int main(int argc, char **argv)
 
     assert(argc == 2);
     dokusha::BPETokenizer<std::string> tokenizer;
-    std::fstream fio;
+    std::ifstream fio;
     std::string line;
     std::chrono::time_point<std::chrono::steady_clock> start, end;
+    std::string buffer;
 
     int filesCompleted = 0;
     for (const auto &textFile : recursive_directory_iterator(argv[1]))
     {
         start = std::chrono::steady_clock::now();
-        fio.open(textFile.path(), std::ios::in);
+        fio.open(textFile.path(), std::ios::binary);
+        fio.seekg(0, std::ios::end);
+        std::streampos fileSize = fio.tellg();
         fio.seekg(0, std::ios::beg);
+
+        // Define a string to hold the binary data with appropriate size
+        buffer.clear();
+        buffer.resize(static_cast<size_t>(fileSize));
+        // Read the binary data into the string buffer
+        fio.read(&buffer[0], fileSize);
+        std::stringstream ss(buffer);
+
         int lineIndex = 0;
 
-        while (fio)
+        while (getline(ss, line))
         {
-            getline(fio, line);
+
             lineIndex++;
             if (lineIndex < 3)
             {
@@ -32,11 +44,10 @@ int main(int argc, char **argv)
             }
             tokenizer.addToCorpus(line);
         }
-        filesCompleted++;
 
-        // if (filesCompleted % 100 == 0) {
-        //     tokenizer.printVocabulary(false);
-        // }
+        // Close the file after reading
+        fio.close();
+        filesCompleted++;
 
         fio.close();
 
@@ -44,29 +55,34 @@ int main(int argc, char **argv)
         std::cout << "\r[Files completed: " << filesCompleted << "], Time(ms):"
                   << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << ", Vocabulary Size: " << tokenizer.getVocabularySize() << " |";
         std::cout.flush();
+        if (filesCompleted % 100 == 0) {
+        break;
+        // tokenizer.pruneWordFrequency();
+        }
+        // break;
     }
     std::cout << std::endl;
     tokenizer.printVocabulary(false);
+    tokenizer.pruneWordList();
+    // tokenizer.printWordWiseTokenList();
+    // print(tokenizer.wordFrequency.size());
 
-    /*
-
-    line =
-        "                               Hello, world! I am IronAB Man... I am "
-        "the savious of thos world. I am a good boy.";
-    tokenizer.addToCorpus(line);
-
-    for (int i = 0; i < 50; i++)
+    while (tokenizer.getVocabularySize() < 1024)
     {
+        start = std::chrono::steady_clock::now();
         tokenizer.runLearningIteration();
+        end = std::chrono::steady_clock::now();
+        std::cout << "\r[Vocab Size: " << tokenizer.getVocabularySize() << "], Time(s):"
+                  << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0 << " |";
+        std::cout.flush();
     }
-    // tokenizer.printVocabulary(true);
-    std::vector<int> tokenizedText = tokenizer.tokenize("I am Iron Man!, hehehehe");
-    std::cout << tokenizer.detokenize(tokenizedText) << std::endl;
+    tokenizer.pruneRedundantTokens();
+    tokenizer.printVocabulary(false);
+    // std::vector<unsigned short> tokenizedText = tokenizer.tokenize("I am Iron Man!, hehehehe");
+    // std::cout << tokenizer.detokenize(tokenizedText) << std::endl;
 
-    std::string s = "abcd";
-    print(tokenizer.detokenize(tokenizer.tokenize(s)));
-
-    */
+    // std::string s = "abcd";
+    // print(tokenizer.detokenize(tokenizer.tokenize(s)));
 
     return 0;
 }
